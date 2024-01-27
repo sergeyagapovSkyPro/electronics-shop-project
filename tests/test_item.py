ad4bacb1fb4d23599626eca7d36beb4ffbbf0fea
Segrey Agapov <stigrov@ya.ru>
2024-01-27 22:37:27 +0300

    fixes test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+from config import ITEMS
 
 
 @pytest.fixture
@@ -23,4 +24,21 @@ def test_apply_discount(item):
     item.apply_discount()
     assert item.price == 8000.0
 
+
+def test_name():
+    """Тест проверки длины наименования товара (не больше 10 символов)"""
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.name == "Смартфон"
+    item1.name = "Духовой шкаф"
+    assert item1.name == "Духовой шк"
+
+def test_instantiate_from_csv():
+    """Проверка добавления экземпляров класса из CSV файла"""
+    Item.instantiate_from_csv(ITEMS)
+    assert len(Item.all) == 5
+
+def test_string_to_number():
+    """Проверка возвращения числа из строки-числа"""
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number("1.5") == 1
 
