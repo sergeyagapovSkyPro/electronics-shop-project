c866b6b47f8502ef7d1c20dbd388763633b4a097
Segrey Agapov <stigrov@ya.ru>
2024-01-27 21:28:35 +0300

    fixes item.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -33,3 +36,30 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """функция проверяет длинну наименования товаров не более 10 символов"""
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        """добовление экземпляров класса из csv файла"""
+        cls.all = []
+        with open(csv_file) as file:
+            data = csv.DictReader(file)
+            for row in data:
+                cls(str(row['name']), float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(str_number):
+        """возвращает число из числа - строки"""
+        number = float(str_number)
+        return int(number)
